1b790c20d185b6e243870dcfd0df7326ac217a0b
swata-dev <74521093+swata-dev@users.noreply.github.com>
2021-03-28 20:01:13 +0900

    color_adjusted

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,30 @@
-import PropTypes from 'prop-types'
-import Button from './ Button'
+import PropTypes from 'prop-types';
+import Button from './ Button';
 
-const Header = ({title}) => {
-    const onClick = (e) => {
-        console.log(e)
-    }
+const Header = ({ title }) => {
+  const onClick = (e) => {
+    console.log(e);
+  };
 
-    return (
-        <header className='header'>
-            <h1 style={headingStyle}>{title}</h1>
-            <Button text='Add' onClick={onClick}/>
-            <Button text='Delete'/>
-        </header>
-    )
-}
+  return (
+    <header className='header'>
+      <h1 style={headingStyle}>{title}</h1>
+      <Button text='Add' onClick={onClick} />
+      <Button text='Delete' />
+    </header>
+  );
+};
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
-
-}
+  title: PropTypes.string.isRequired,
+};
 
 // CSS in JS
 const headingStyle = {
-    color: 'white',
-    backgroundColor: 'green',
-    padding: '10px'
-}
+  color: 'gold',
+  backgroundColor: 'green',
+  padding: '15px',
+  margin: '20px',
+};
 
-export default Header
+export default Header;
