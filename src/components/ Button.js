bcc9679e4174745eb3a314703612f67671e794cc
swata-dev <74521093+swata-dev@users.noreply.github.com>
2021-03-28 19:53:34 +0900

    botton_color_changed

--- src/components/ Button.js
+++ src/components/ Button.js
@@ -1,23 +1,27 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
-const  Button = ({color, text, onClick}) => {
-
-    return (
-    <button onClick={onClick} style={{
-        backgroundColor: color}} className='btn'>
-        {text}
+const Button = ({ color, text, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      style={{
+        backgroundColor: color,
+      }}
+      className='btn'
+    >
+      {text}
     </button>
-    )
-}
+  );
+};
 
 Button.defaultProps = {
-    color: 'steelblue'
-}
+  color: 'green',
+};
 
 Button.propsTypes = {
-    color: PropTypes.string,
-    text: PropTypes.string,
-    onClick: PropTypes.func,
-}
+  color: PropTypes.string,
+  text: PropTypes.string,
+  onClick: PropTypes.func,
+};
 
-export default  Button
+export default Button;
